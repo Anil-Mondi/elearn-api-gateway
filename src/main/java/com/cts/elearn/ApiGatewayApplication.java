9195d4394c9ca66663dc171b04ed32f5631752bb
moanil <4188manil@gmail.com>
2026-05-28 14:04:57 +0530

    Add startup log message to API Gateway

--- src/main/java/com/cts/elearn/ApiGatewayApplication.java
+++ src/main/java/com/cts/elearn/ApiGatewayApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
     public static void main(String[] args) {
+        System.out.println("Starting API Gateway...");
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 }
